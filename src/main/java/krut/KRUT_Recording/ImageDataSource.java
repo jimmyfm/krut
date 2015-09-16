f913b6d1306c610511004dab0f2d30094c5185e5
Luigi P <luigi.polvani@gmail.com>
2015-09-15 22:23:00 -0500

    Inner classes came out plus some hacking to cut references between classes

---
+++ src/main/java/krut/KRUT_Recording/ImageDataSource.java
@@ -0,0 +1,75 @@
+package krut.KRUT_Recording;
+
+import javax.media.MediaLocator;
+import javax.media.Time;
+import javax.media.protocol.ContentDescriptor;
+import javax.media.protocol.PullBufferDataSource;
+import javax.media.protocol.PullBufferStream;
+
+/**
+ * A DataSource to read from a list of JPEG image files and
+ * turn that into a stream of JMF buffers.
+ * The DataSource is not seekable or positionable.
+ */
+class ImageDataSource extends PullBufferDataSource {
+
+    PullBufferStream streams[];
+
+    ImageDataSource(int width, int height, int frameRate, DataList data) {
+        streams = new PullBufferStream[1];
+        streams[0] = new ImageSourceStream(width, height, frameRate, data);
+    }
+
+    public void setLocator(MediaLocator source) {
+    }
+
+    public MediaLocator getLocator() {
+        return null;
+    }
+
+    /**
+     * Content type is of RAW since we are sending buffers of video
+     * frames without a container format.
+     */
+    public String getContentType() {
+        return ContentDescriptor.RAW;
+    }
+
+    public void connect() {
+    }
+
+    public void disconnect() {
+    }
+
+    public void start() {
+    }
+
+    public void stop() {
+    }
+
+
+    /**
+     * Return the ImageSourceStreams.
+     */
+    public PullBufferStream[] getStreams() {
+        return streams;
+    }
+
+    /**
+     * We could have derived the duration from the number of
+     * frames and frame rate.  But for the purpose of this program,
+     * it's not necessary.
+     */
+    public Time getDuration() {
+        return DURATION_UNKNOWN;
+    }
+
+    public Object[] getControls() {
+        return new Object[0];
+    }
+
+    public Object getControl(String type) {
+        return null;
+    }
+}
+    
---
+++ src/main/java/krut/KRUT_Recording/ImageSourceStream.java
@@ -0,0 +1,161 @@
+package krut.KRUT_Recording;
+
+import javax.media.Buffer;
+import javax.media.Format;
+import javax.media.format.VideoFormat;
+import javax.media.protocol.ContentDescriptor;
+import javax.media.protocol.PullBufferStream;
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * The source stream to go along with ImageDataSource.
+ */
+class ImageSourceStream implements PullBufferStream {
+
+    private DataList JPGImages;
+    int width, height;
+    VideoFormat format;
+    float frameRate;
+    long seqNo = 0;
+
+    boolean ended = false;
+
+    public ImageSourceStream(int width, int height, int frameRate, DataList data) {
+        this.width = width;
+        this.height = height;
+        this.frameRate = (float) frameRate;
+        this.JPGImages = data;
+            
+          /* The commented out code below is remains from a
+           * failed attempt to include avi output. The code is
+           * left in the source like this as a reminder to the
+           * author
+           */
+//            format = new VideoFormat(VideoFormat.JPEG,
+//                    new Dimension(width, height),
+//                    Format.NOT_SPECIFIED,
+//                    Format.byteArray,
+//                    (float)frameRate);
+
+        format = new VideoFormat(VideoFormat.JPEG,
+                new Dimension(width, height),
+                Format.NOT_SPECIFIED,
+                Format.byteArray,
+                (float) frameRate);
+
+            
+          /* The commented out code below is remains from a
+           * failed attempt to include avi output. The code is
+           * left in the source like this as a reminder to the
+           * author
+           */
+//            final int rMask = 0x00ff0000;
+//            final int gMask = 0x0000FF00;
+//            final int bMask = 0x000000ff;
+
+//            format =
+//                new javax.media.format.RGBFormat(
+//                    new Dimension(width, height),
+//                    Format.NOT_SPECIFIED,
+//                    Format.intArray,
+//                    frameRate,
+//                    24,
+//                    rMask,
+//                    gMask,
+//                    bMask);
+    }
+
+    /**
+     * We should never need to block assuming data are read from files.
+     */
+    public boolean willReadBlock() {
+        return false;
+    }
+
+    /**
+     * This is called from the Processor to read a frame worth
+     * of video data.
+     */
+    public void read(Buffer buf) throws IOException {
+
+        // Check if we've finished all the frames.
+        if (JPGImages.finished) {
+            // We are done.  Set EndOfMedia.
+            System.err.println("Done reading all images.");
+            System.err.println("Frames: " + JPGImages.totalPics);
+            System.err.println("Missed frames: " +
+                    (JPGImages.sentPics - JPGImages.totalPics));
+            buf.setEOM(true);
+            buf.setOffset(0);
+            buf.setLength(0);
+            ended = true;
+            return;
+        }
+
+        float time1 = seqNo * (1000 / frameRate) * 1000000;
+        long time = (long) time1;
+        buf.setTimeStamp(time);
+
+        buf.setSequenceNumber(seqNo++);
+
+        byte[] picBytes = JPGImages.readNode();            // read the next image in line
+        // in the DataList.
+        byte data[] = null;
+
+//            int data[] = new int[picBytes.length / 4];
+
+        // Read the entire JPEG image from the file.
+        data = picBytes;
+
+          /* The commented out code below is remains from a
+           * failed attempt to include avi output. The code is
+           * left in the source like this as a reminder to the
+           * author
+           */
+//            int dataCnt = 0;
+//            int mult;
+//            for (int cnt = 0; cnt < data.length; cnt ++) {
+//                mult = 256*256*256;
+//                for (int loopCnt = 0; loopCnt < 4; loopCnt++) {
+//                    data[picCnt] += picBytes[dataCnt++] * mult;
+//                    mult /= 256;
+//                }
+//            }
+        buf.setData(data);
+
+
+        buf.setOffset(0);
+        buf.setLength((int) picBytes.length);
+        buf.setFormat(format);
+        buf.setFlags(buf.getFlags() | buf.FLAG_KEY_FRAME);
+
+    }
+
+    /**
+     * Return the format of each video frame.  That will be JPEG.
+     */
+    public Format getFormat() {
+        return format;
+    }
+
+    public ContentDescriptor getContentDescriptor() {
+        return new ContentDescriptor(ContentDescriptor.RAW);
+    }
+
+    public long getContentLength() {
+        return 0;
+    }
+
+    public boolean endOfStream() {
+        return ended;
+    }
+
+    public Object[] getControls() {
+        return new Object[0];
+    }
+
+    public Object getControl(String type) {
+        return null;
+    }
+}
